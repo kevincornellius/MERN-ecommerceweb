6a9dbd138d5c9717d58a17eec52f367e0e1974e1
kevincornellius <kevincornelius1905@gmail.com>
2024-07-23 19:07:00 +0700

    multer test

--- backend/index.js
+++ backend/index.js
@@ -46,23 +46,23 @@ app.get("/", (req, res) => {
 
 //Image Storage
 
-const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: './upload/images',
+//     filename: (req, file, cb) => {
+//         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+//     }
+// })
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage })
 
-app.use('/images', express.static('upload/images'))
+// app.use('/images', express.static('upload/images'))
 
-app.post("/upload", upload.single('product'), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://${req.hostname}:/images/${req.file.filename}`,
-    })
-})
+// app.post("/upload", upload.single('product'), (req, res) => {
+//     res.json({
+//         success: 1,
+//         image_url: `http://${req.hostname}:/images/${req.file.filename}`,
+//     })
+// })
 
 
 //Product Schema MongoDB
